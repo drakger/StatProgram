5829acc1ba286b2818045d7f5bdf849fda1cbc5d
drakger <r.geri@t-online.hu>
2013-08-01 17:35:13 +0200

    GlobalMouseHook etc

--- StatProgramProject/StatProgramProject/Form1.Designer.cs
+++ StatProgramProject/StatProgramProject/Form1.Designer.cs
@@ -36,18 +36,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
             this.contextMenuStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -76,7 +78,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 91);
+            this.label1.Location = new System.Drawing.Point(240, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(22, 24);
             this.label1.TabIndex = 1;
@@ -86,7 +88,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(428, 118);
+            this.label2.Location = new System.Drawing.Point(601, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 24);
             this.label2.TabIndex = 2;
@@ -95,22 +97,14 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(18, 13);
+            this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(174, 24);
+            this.label3.Size = new System.Drawing.Size(231, 71);
             this.label3.TabIndex = 3;
             this.label3.Text = "Mouse statistics";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(20, 14);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(204, 24);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Keyboard statistics";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // menuStrip1
             // 
@@ -118,7 +112,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(499, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(690, 24);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -141,52 +135,110 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(18, 91);
+            this.label5.Location = new System.Drawing.Point(3, 71);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(78, 24);
+            this.label5.Size = new System.Drawing.Size(120, 24);
             this.label5.TabIndex = 6;
-            this.label5.Text = "Clicks:";
+            this.label5.Text = "Left clicks:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(257, 118);
+            this.label6.Location = new System.Drawing.Point(477, 71);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(142, 24);
+            this.label6.Size = new System.Drawing.Size(98, 48);
             this.label6.TabIndex = 7;
             this.label6.Text = "Key presses:";
             // 
-            // panel1
+            // label7
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 27);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(219, 383);
-            this.panel1.TabIndex = 8;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Right clicks:";
             // 
-            // panel3
+            // label8
             // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.label4);
-            this.panel3.Location = new System.Drawing.Point(237, 27);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(245, 383);
-            this.panel3.TabIndex = 9;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(240, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 24);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(3, 303);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(146, 24);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Middle clicks:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(240, 303);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 24);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "0";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 124F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.label10, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.00394F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.83826F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 507);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(477, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 48);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Keyboard statistics";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 431);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(690, 546);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -196,10 +248,8 @@
             this.contextMenuStrip1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,14 +263,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- StatProgramProject/StatProgramProject/Form1.cs
+++ StatProgramProject/StatProgramProject/Form1.cs
@@ -30,13 +30,19 @@ namespace WindowsFormsApplication2
         }
         private void Mouse(object sender, EventArgs e)
         {
-            vmi.lclick++;
-            label1.Text = Convert.ToString(vmi.lclick);
+            Updatestats();
+        }
+        public void Updatestats()
+        {
+            label1.Text = Convert.ToString(vars.lclick);
+            label8.Text = Convert.ToString(vars.rclick);
+            label10.Text = Convert.ToString(vars.mclick);
+            label2.Text = Convert.ToString(vars.keys);
         }
         public void gHook_KeyUp(object sender, KeyEventArgs e)
         {
-            vmi.keys++;
-            label2.Text = Convert.ToString(vmi.keys);
+            vars.keys++;
+            Updatestats();
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -67,129 +73,33 @@ namespace WindowsFormsApplication2
 
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            vmi.exit = true;
+            vars.exit = true;
             this.Close();
         }
 
-        public static class vmi
+        public static class vars
         {
             public static int rclick;
             public static int lclick;
+            public static int mclick;
             public static int keys;
             public static bool exit;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (vmi.exit == false)
+            if (vars.exit == false)
             {
                 this.WindowState = FormWindowState.Minimized;
                 this.Hide();
                 e.Cancel = true;
             }
         }
-        public static class MouseHook
-        {
-            public static event EventHandler MouseAction = delegate { };
-
-            public static void Start()
-            {
-                _hookID = SetHook(_proc);
-
-
-            }
-            public static void stop()
-            {
-                UnhookWindowsHookEx(_hookID);
-            }
-
-            private static LowLevelMouseProc _proc = HookCallback;
-            private static IntPtr _hookID = IntPtr.Zero;
-
-            private static IntPtr SetHook(LowLevelMouseProc proc)
-            {
-                using (Process curProcess = Process.GetCurrentProcess())
-                using (ProcessModule curModule = curProcess.MainModule)
-                {
-                    return SetWindowsHookEx(WH_MOUSE_LL, proc,
-                      GetModuleHandle(curModule.ModuleName), 0);
-                }
-            }
-
-            private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-            private static IntPtr HookCallback(
-              int nCode, IntPtr wParam, IntPtr lParam)
-            {
-                if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)  // Different Mouse Clicks
-                {
-                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                    MouseAction(null, new EventArgs());
-                }
-                if (nCode >= 0 && MouseMessages.WM_RBUTTONDOWN == (MouseMessages)wParam)
-                {
-                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                    MouseAction(null, new EventArgs());
-                }
-                if (nCode >= 0 && MouseMessages.WM_MBUTTONDOWN == (MouseMessages)wParam)
-                {
-                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                    MouseAction(null, new EventArgs());
-                }
-                return CallNextHookEx(_hookID, nCode, wParam, lParam);
-            }
-
-            private const int WH_MOUSE_LL = 14;
-
-            private enum MouseMessages
-            {
-                WM_LBUTTONDOWN = 0x0201,
-                WM_LBUTTONUP = 0x0202,
-                WM_MOUSEMOVE = 0x0200,
-                WM_MOUSEWHEEL = 0x020A,
-                WM_RBUTTONDOWN = 0x0204,
-                WM_MBUTTONDOWN = 0x0207,
-                WM_RBUTTONUP = 0x0205
-            }
-            [StructLayout(LayoutKind.Sequential)]
-            private struct POINT
-            {
-                public int x;
-                public int y;
-            }
-
-            [StructLayout(LayoutKind.Sequential)]
-            private struct MSLLHOOKSTRUCT
-            {
-                public POINT pt;
-                public uint mouseData;
-                public uint flags;
-                public uint time;
-                public IntPtr dwExtraInfo;
-            }
-
-            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            private static extern IntPtr SetWindowsHookEx(int idHook,
-              LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
-
-            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-              IntPtr wParam, IntPtr lParam);
-
-            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        }
 
         private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            vmi.exit = true;
+            vars.exit = true;
             this.Close();
         }
-
     }
 }
